ff4a6107cc00785be6ceaf95ea87251cbac59aac
Sergey Shulga <shul.sergio@gmail.com>
2024-09-15 23:57:57 +0300

    v 1.1 add table + start app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,23 @@
-
-import './App.css'
+import { useState } from "react";
+import Container from "../Container/Container";
+import Loader from "../Loader/Loader";
+import Section from "../Section/Section";
+import "./App.css";
+import { useTable } from "react-table";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
 
   return (
     <>
-      <p>Stydy helper</p>
-
+      <Section>
+        <Container>
+          {isLoading && <Loader />}
+          <p>Stydy helper</p>
+        </Container>
+      </Section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
